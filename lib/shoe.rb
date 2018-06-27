9c6b1971471c7f7fc3bd07e2b2e21853b55ddb38
Andres Manco <andresmanco@gmail.com>
2018-06-27 15:42:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,11 @@
 class Shoe
-  def initialize(brand)
-    @brand = brand
-  end
+
   attr_reader :reader
   attr_accessor :color, :size, :material, :condition, :brand
   
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
 end
