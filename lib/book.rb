75a97d40982c31740ff8a6d2816c214d9d934989
Andres Manco <andresmanco@gmail.com>
2018-06-27 15:31:49 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,28 +1,35 @@
 class Book
+  
   def initialize(title)
     @title = title
   end
   def title
     @title
   end
+  
   def author=(name)
     @author = name
   end
   def author
     @author
   end
+  
   def page_count= (count)
     @page_count = count
   end
   def page_count
     @page_count
   end
+  
   def genre= (book_genre)
     @genre = book_genre
   end
   def genre
     @genre
   end
+  
+  def turn_page
+    
   
   
 
